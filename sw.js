c6abb137d4e35a96ca822c8fa9f251a683ff6ae6
juanmesia <juanmesiaguerra@gmail.com>
2026-02-24 23:52:58 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "jmn-calc-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
